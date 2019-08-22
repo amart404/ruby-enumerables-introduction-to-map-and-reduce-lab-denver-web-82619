b372ed034f1d3eb61bb39bd54a63a179a08b3821
Aaron Martinez <aaronmarti17@gmail.com>
2019-08-22 05:37:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,9 +50,12 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  new_array = source_array.map{|e| !!e}
   counter = 0
-  while source_array[counter] do 
-    return false if source_array[counter] == false
+  while new_array[counter] do 
+    if new_array[counter] == false
+      return false
+    end
     counter += 1 
   end
   return true
